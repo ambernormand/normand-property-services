14e2fe8c9b44244ae4699d2a69d70def338734f0
thecomputer <thecomputer@thecomputers-iMac.local>
2021-02-25 21:40:08 -0600

    init and plop a template

---
+++ normand-property-services/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+
+import Homepage from './Homepage';
+
+function App() {
+  return (
+    <Homepage />
+  );
+}
+
+export default App;
